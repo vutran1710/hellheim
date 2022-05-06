e5db3432471c0d3bf69cd1ffa6ab544d8fcb89ec
Vu Tran <me@vutr.io>
2022-05-06 14:55:19 +0700

    note

--- src/servers/tcp.rs
+++ src/servers/tcp.rs
@@ -6,6 +6,7 @@ use tokio::net::TcpStream;
 type GenericResult<T> = Result<T, Box<dyn Error>>;
 
 pub async fn tcp_init() -> GenericResult<()> {
+    // TODO: pass port via env
     let addr = "127.0.0.1:8080".to_string();
     let server = TcpListener::bind(&addr).await?;
 
