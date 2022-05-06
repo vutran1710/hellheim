12a05ff63c9e191e2e1ff9212eda54c98f537064
Vu Tran <me@vutr.io>
2022-05-06 22:50:12 +0700

    hyper server

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
-mod servers;
+use hyper::service::make_service_fn;
+use hyper::service::service_fn;
+use hyper::Body;
+use hyper::Request;
+use hyper::Response;
+use hyper::Server;
+use std::convert::Infallible;
+use std::net::SocketAddr;
 
-use servers::tcp::*;
-use tokio::net::TcpStream;
-
-async fn handle_stream(s: TcpStream) -> () {
-    println!("-------------------- {:?}", s);
+async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    Ok(Response::new("Hello, World".into()))
 }
 
 #[tokio::main]
-async fn main() -> GenericResult<()> {
-    tcp_init(handle_stream).await
+async fn main() {
+    // TODO: use --port from env
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let make_svc = make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(hello_world)) });
+    let server = Server::bind(&addr).serve(make_svc);
+
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
 }
